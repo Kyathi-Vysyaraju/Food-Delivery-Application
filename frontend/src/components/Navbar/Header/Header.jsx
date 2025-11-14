e33b654dcd2b1d077092852b469fbbdbb572ce80
Kyathi Vysyaraju <kyathivysyaraju@Kyathis-MacBook-Air.local>
2025-11-14 22:31:20 +0530

    first commit

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Hungry for something different? Dive into a world of delicious possibilities — from local favorites to global cuisines, freshly prepared and delivered right to your doorstep. Discover, order, and enjoy — all in one place.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
